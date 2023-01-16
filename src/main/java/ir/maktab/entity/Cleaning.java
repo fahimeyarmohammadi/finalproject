aa276e542474f18e82980665d424f4daeb72cbf1
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-17 01:03:39 +0330

    add Cleaning.java class

---
+++ src/main/java/ir/maktab/entity/Cleaning.java
@@ -0,0 +1,16 @@
+package ir.maktab.entity;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.Entity;
+
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Cleaning extends Service {
+}
