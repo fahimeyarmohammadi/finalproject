128191f2637a179fbed4ad1b9af7b7a90fe8f8be
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-17 00:22:43 +0330

    add Person.java class body

--- src/main/java/ir/maktab/entity/Person.java
+++ src/main/java/ir/maktab/entity/Person.java
@@ -1,4 +1,36 @@
 package ir.maktab.entity;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PROTECTED)
+@MappedSuperclass
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    String name;
+
+    @Column(nullable = false)
+    String familyName;
+
+    @Column(nullable = false, unique = true)
+    String email;
+
+    @Column(nullable = false)
+    String password;
+
+    @Temporal(value = TemporalType.DATE)
+    Date date;
 }
