4ced91da99faeea4bd70d8834032f90e4523e605
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-17 01:54:19 +0330

    add Opinion.java class body

--- src/main/java/ir/maktab/entity/Opinion.java
+++ src/main/java/ir/maktab/entity/Opinion.java
@@ -1,4 +1,29 @@
 package ir.maktab.entity;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Opinion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    int score;
+    String comment;
+
+     Expert expert;
+
+     User user;
 }
