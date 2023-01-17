311f91e8dee9c479c0a7b3bf5de8326594cbecc4
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-17 19:38:53 +0330

    add SubServiceService.java class

---
+++ src/main/java/ir/maktab/service/SubServiceService.java
@@ -0,0 +1,25 @@
+package ir.maktab.service;
+
+import ir.maktab.entity.BaseService;
+import ir.maktab.entity.SubService;
+import ir.maktab.repository.BaseServiceRepository;
+import ir.maktab.repository.SubServiceRepository;
+
+public class SubServiceService {
+
+    private static final SubServiceService subServiceService = new SubServiceService();
+
+    private SubServiceService() {
+    }
+
+    public static SubServiceService getInstance() {
+        return subServiceService;
+    }
+
+    private final SubServiceRepository subServiceRepository = SubServiceRepository.getInstance();
+
+    public void addSubService(SubService subService) {
+
+        subServiceRepository.save(subService);
+    }
+}
