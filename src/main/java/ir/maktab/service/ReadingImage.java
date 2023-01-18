a9631d6d74bd03de0b498e3bc6772292a9bbd3ad
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-18 15:07:26 +0330

    add ReadingImage.java class body

--- src/main/java/ir/maktab/service/ReadingImage.java
+++ src/main/java/ir/maktab/service/ReadingImage.java
@@ -1,4 +1,20 @@
 package ir.maktab.service;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class ReadingImage {
+
+    public ReadingImage() throws IOException {
+    }
+
+    String imagePath = "path/to/your/image.jpg";
+    BufferedImage myPicture = ImageIO.read(new File(imagePath));
+
+
+
+
 }
+
